6a920f9b4982a1bb1970742fa67c44a82b92a7c0
Phillip Schackier <CppPhil@users.noreply.github.com>
2021-10-16 14:21:44 +0200

    Initial commit

---
+++ JSON/main.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+
+// JSON (JavaScript Object Notation)
+// XML
+// REST
+// RPC (remote procedure call) / JSON-RPC
+// Save configuration data (.ini)
+
+int main()
+{
+	std::cout << "Hello CMake." << std::endl;
+	return 0;
+}
